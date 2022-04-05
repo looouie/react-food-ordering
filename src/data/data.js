54cb5bdaac41ec900c5515fc970fc9800133f15f
loyilulu <loyichan2@gmail.com>
2022-04-05 16:09:54 +0200

    added data file

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+import ciabatta from "../assets/ciabattaBread.png";
+import oatBread from "../assets/oatBread.png";
+import raisinBread from "../assets/raisinBread.png";
+import sourdoughBread from "../assets/sourdoughBread.png";
+
+import lyxSemla from "../assets/LyxSemla.png";
+import wieneroll from "../assets/wieneroll.png";
+import cinnamonBun from "../assets/cinnamonbun.png";
+import croissant from "../assets/croissant.png";
+
+import princessCake from "../assets/princessCake.png";
+import budapestCake from "../assets/budapestCake.png";
+
+export const breadList = [
+  {
+    id: "bread-01",
+    name: "Ciabatta",
+    description: "Italian white bread ",
+    price: 15,
+    image: ciabatta,
+  },
+  {
+    id: "bread-02",
+    name: "Oatmeal Bread",
+    description: "Homemade oatmeal wheat bread",
+    price: 35,
+    image: oatBread,
+  },
+  {
+    id: "bread-03",
+    name: "Raisin Rye bread",
+    description: "Homemade rye bread with raisin",
+    price: 55,
+    image: raisinBread,
+  },
+  {
+    id: "bread-04",
+    name: "Sourdough bread",
+    description: "Sourdough bread",
+    price: 55,
+    image: sourdoughBread,
+  },
+];
+
+export const fikaList = [
+  {
+    id: "fika-01",
+    name: "Semla",
+    description: "Traditional Swedish semla",
+    price: 48,
+    image: lyxSemla,
+  },
+  {
+    id: "fika-02",
+    name: "Wieneroll",
+    description: "Wieneroll",
+    price: 35,
+    image: wieneroll,
+  },
+  {
+    id: "fika-03",
+    name: "Cinnamon bun",
+    description: "Traditional Swedish cinnamon bun",
+    price: 35,
+    image: cinnamonBun,
+  },
+  {
+    id: "fika-04",
+    name: "Croissant",
+    description: "Croissant",
+    price: 28,
+    image: croissant,
+  },
+];
+
+export const cakeList = [
+  {
+    id: "cake-01",
+    name: "Princess Cake",
+    description:
+      "Traditional Swedish cake layer by sponge cake, whipped cream, raspberry jam and covered by marzipan",
+    price: 45,
+    image: princessCake,
+  },
+  {
+    id: "cake-02",
+    name: "Budapest roll",
+    description: "Swedish Budapest roll with hazelnut meringue",
+    price: 45,
+    image: budapestCake,
+  },
+];
