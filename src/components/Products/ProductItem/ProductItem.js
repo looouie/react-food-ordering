6b71dd9aa9b9b2c44ba2c29af7ac3f3526bed98b
loyilulu <loyichan2@gmail.com>
2022-04-05 10:38:59 +0200

    added dummy data to Product and added Product to APP

--- src/components/Products/ProductItem/ProductItem.js
+++ src/components/Products/ProductItem/ProductItem.js
@@ -1,3 +1,27 @@
-const ProductItem = () => {};
+import classes from "./ProductItem.module.css";
+import cinnamonBun from "../../../assets/cinnamonbun.png";
+
+const ProductItem = () => {
+  const dummy = {
+    id: "bread-01",
+    name: "Cinnamon bun",
+    description: "delicious",
+    amount: 0,
+    price: 10,
+  };
+
+  return (
+    <li className={classes.product}>
+      <div className={classes.productImage}>
+        <img src={cinnamonBun} alt="product" />
+      </div>
+      <div className={classes.productDescription}>
+        <h3>{dummy.name}</h3>
+        <div className={classes.description}>{dummy.description}</div>
+        <div className={classes.price}>{dummy.price}</div>
+      </div>
+    </li>
+  );
+};
 
 export default ProductItem;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,3 +1,18 @@
-const Products = () => {};
+import classes from "./Products.module.css";
+import ProductItem from "./ProductItem/ProductItem";
+
+const Products = () => {
+  return (
+    <>
+      <section className={classes.products}>
+        <h2 className={classes.categoryName}>Bread</h2>
+        <ul>
+          <ProductItem />
+          <ProductItem />
+        </ul>
+      </section>
+    </>
+  );
+};
 
 export default Products;
