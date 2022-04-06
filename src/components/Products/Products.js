47cd8314ed18654626231dbd795446c4c7b7d30b
loyilulu <loyichan2@gmail.com>
2022-04-06 13:52:26 +0200

    css changes in Products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,12 @@
 import classes from "./Products.module.css";
 import ProductItem from "./ProductItem/ProductItem";
+import background from "../../assets/cover-vertical.jpeg";
 
 import { breadList, fikaList, cakeList } from "../../data/data";
 
 const Products = () => {
   return (
-    <>
+    <div className={classes.container}>
       <section className={classes.products}>
         <h2 className={classes.categoryName}>Bread</h2>
         <ul>
@@ -24,7 +25,7 @@ const Products = () => {
           <ProductItem productList={cakeList} />
         </ul>
       </section>
-    </>
+    </div>
   );
 };
 
