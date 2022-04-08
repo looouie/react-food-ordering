2f9841e769d68245f84b27bb7834cd3484053309
loyilulu <loyichan2@gmail.com>
2022-04-08 17:56:34 +0200

    install react-redux and apply in index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
-import CartProvider from "./components/store/CartProvider";
 
 function App() {
   return (
-    <CartProvider>
+    <>
       <Header />
       <p>order now!</p>
       <Products />
-    </CartProvider>
+    </>
   );
 }
 
