ac9fc7dcb34de52ccc092c4ad5eb8ddc61787043
loyilulu <loyichan2@gmail.com>
2022-04-08 16:56:30 +0200

    elaborate the CartProvider structure

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,3 +1,15 @@
-const CartProvider = () => {};
+import { useState } from "react";
+
+const defaultState = {
+  product: [],
+  totalAmount: 0,
+  totalPrice: 0,
+};
+
+const cartReducer = () => {};
+
+const CartProvider = () => {
+  const [cartState, setCartState] = useState(cartReducer, defaultState);
+};
 
 export default CartProvider;
