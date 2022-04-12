9470076610b7a8d27ffe61cf5308c7ea42847d23
loyilulu <loyichan2@gmail.com>
2022-04-12 10:03:23 +0200

    update Overlay component and its css

--- src/components/UI/Overlay/Overlay.js
+++ src/components/UI/Overlay/Overlay.js
@@ -1,5 +1,17 @@
 import classes from "./Overlay.module.css";
 
-const Overlay = () => {};
+const Overlay = () => {
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} />;
+  };
+
+  const OverlayModal = (props) => {
+    return (
+      <div className={classes.overlayModal}>
+        <div className={classes.content}>{props.children}</div>
+      </div>
+    );
+  };
+};
 
 export default Overlay;
