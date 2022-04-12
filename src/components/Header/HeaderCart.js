4482f2f52cd236483b9b9cbf411871434b438d78
loyilulu <loyichan2@gmail.com>
2022-04-12 21:59:56 +0200

    passed the onClick props into HeaderCart

--- src/components/Header/HeaderCart.js
+++ src/components/Header/HeaderCart.js
@@ -1,11 +1,10 @@
 import classes from "./HeaderCart.module.css";
 import { BsCart4 } from "react-icons/bs";
 
-const HeaderCart = () => {
+const HeaderCart = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <BsCart4 className={classes.cartIcon} />
-      {/* <span>Cart</span> */}
       <span className={classes.badge}>0</span>
     </button>
   );
