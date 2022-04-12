3fc8b69168da5c950aff6032d3f5086d2b6d403e
loyilulu <loyichan2@gmail.com>
2022-04-12 21:59:23 +0200

    dispatch setShowCart action in Header and pass it to onClick to HeaderCart button component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,21 @@ import classes from "./Header.module.css";
 import HeaderCart from "./HeaderCart";
 import baking from "../../assets/baking.jpg";
 
+import { useDispatch } from "react-redux";
+import { showCartActions } from "../../store/showCart-slice";
+
 const Header = (props) => {
+  const dispatch = useDispatch();
+
+  const showCartHandler = () => {
+    dispatch(showCartActions.setShowCart());
+  };
+
   return (
     <>
       <header className={classes.header}>
         <p>Louie's Bakery</p>
-        <HeaderCart />
+        <HeaderCart onClick={showCartHandler} />
       </header>
       <div className={classes.headerImage}>
         <img className={classes.desktopImage} src={baking} alt="HeaderImage" />
