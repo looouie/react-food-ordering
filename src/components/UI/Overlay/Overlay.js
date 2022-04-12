0292627f1b459e0fd61837bb19d3cec7ecaf8fa7
loyilulu <loyichan2@gmail.com>
2022-04-12 10:23:41 +0200

    update overlay component

--- src/components/UI/Overlay/Overlay.js
+++ src/components/UI/Overlay/Overlay.js
@@ -1,32 +1,30 @@
 import ReactDOM from "react-dom";
 import classes from "./Overlay.module.css";
 
-const Overlay = () => {
-  const Backdrop = (props) => {
-    return <div className={classes.backdrop} />;
-  };
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
 
-  const OverlayModal = (props) => {
-    return (
-      <div className={classes.overlayModal}>
-        <div className={classes.overlayContent}>{props.children}</div>
-      </div>
-    );
-  };
+const OverlayModal = (props) => {
+  return (
+    <div className={classes.overlayModal}>
+      <div className={classes.overlayContent}>{props.children}</div>
+    </div>
+  );
+};
 
-  const portalEl = document.getElementById("overlay");
+const portalEl = document.getElementById("overlay");
 
-  const Overlay = (props) => {
-    return (
-      <>
-        {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalEl)}
-        {ReactDOM.createPortal(
-          <OverlayModal>{props.children}</OverlayModal>,
-          portalEl
-        )}
-      </>
-    );
-  };
+const Overlay = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalEl)}
+      {ReactDOM.createPortal(
+        <OverlayModal>{props.children}</OverlayModal>,
+        portalEl
+      )}
+    </>
+  );
 };
 
 export default Overlay;
