018206c6051126f8c16743964e62ded12d62b268
loyilulu <loyichan2@gmail.com>
2022-04-12 22:00:49 +0200

    added Cart component in App and show it when state is true

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
+// import Overlay from "./components/UI/Overlay/Overlay";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const cart = useSelector((state) => state.showCart.showCart);
+
   return (
     <>
+      {cart && <Cart />}
       <Header />
       <p className="p">order now!</p>
       <Products />
