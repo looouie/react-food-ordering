89606b62ae78eab4d7abbfb2458eab7d0a55e917
loyilulu <loyichan2@gmail.com>
2022-04-12 21:58:25 +0200

    dispatch setHideCart action in Cart component and pass it as onClose to Overlay

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,15 @@
 import classes from "./Cart.module.css";
 import Overlay from "../UI/Overlay/Overlay";
+import { useDispatch } from "react-redux";
+import { showCartActions } from "../../store/showCart-slice";
 
 const Cart = (props) => {
-  return <Overlay onClose={props.onClose}></Overlay>;
+  const dispatch = useDispatch();
+
+  const closeCartHandler = () => {
+    dispatch(showCartActions.setHideCart());
+  };
+
+  return <Overlay onClose={closeCartHandler} />;
 };
 export default Cart;
