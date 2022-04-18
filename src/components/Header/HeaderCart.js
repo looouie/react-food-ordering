166344e54003a381088cd80587f375c58822416e
loyilulu <loyichan2@gmail.com>
2022-04-18 12:13:54 +0200

    set bumpy effect in badge when totalAmount change

--- src/components/Header/HeaderCart.js
+++ src/components/Header/HeaderCart.js
@@ -1,11 +1,30 @@
 import classes from "./HeaderCart.module.css";
 import { BsCart4 } from "react-icons/bs";
+import { useState, useEffect } from "react";
 
 const HeaderCart = (props) => {
+  const { onClick, totalAmount } = props;
+  const [bumpy, setBumpy] = useState(false);
+
+  useEffect(() => {
+    if (totalAmount === 0) {
+      return;
+    }
+    setBumpy(true);
+
+    const timer = setTimeout(() => {
+      setBumpy(false);
+    }, 400);
+
+    return () => {};
+  }, [totalAmount]);
+
+  const bumpEffect = bumpy ? classes.bump : "";
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={classes.button} onClick={onClick}>
       <BsCart4 className={classes.cartIcon} />
-      <span className={classes.badge}>{props.totalAmount}</span>
+      <span className={classes.badge + " " + bumpEffect}>{totalAmount}</span>
     </button>
   );
 };
