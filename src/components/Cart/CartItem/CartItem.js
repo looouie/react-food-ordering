71548f6a8445d516100c31f03cd314254e7302eb
loyilulu <loyichan2@gmail.com>
2022-04-21 22:17:31 +0200

    added ChangeForm to CartItem and its css

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -1,4 +1,5 @@
 import classes from "./CartItem.module.css";
+import ChangeForm from "../../UI/ChangeForm/ChangeForm";
 
 const CartItem = (props) => {
   const { id, name, price, amount } = props.item;
@@ -9,22 +10,15 @@ const CartItem = (props) => {
   //Decreament Handler
 
   return (
-    <li key={id}>
+    <li key={id} className={classes.itemContainer}>
       <div className={classes.summary}>
         <h3>{name}</h3>
         <div>
-          <span>{price}</span>
+          <span>$ {price}</span>
         </div>
       </div>
       <div className={classes.amount}>
-        <input
-          className={classes.input}
-          id={id}
-          type="number"
-          defaultValue={amount}
-          min="0"
-          max="99"
-        ></input>
+        <ChangeForm id={id} amount={amount} />
       </div>
     </li>
   );
