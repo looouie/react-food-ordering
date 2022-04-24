a07f0490efad4f08086f8de39010341b1bbfd81c
loyilulu <loyichan2@gmail.com>
2022-04-24 16:54:32 +0200

    show total price in Cart and css update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import classes from "./Cart.module.css";
 import Overlay from "../UI/Overlay/Overlay";
 import CartItem from "./CartItem/CartItem";
+import { BsCart4 } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { showCartActions } from "../../store/showCart-slice";
 
@@ -8,6 +9,7 @@ const Cart = (props) => {
   const dispatch = useDispatch();
 
   const cartList = useSelector((state) => state.cart.products);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
 
   const closeCartHandler = () => {
     dispatch(showCartActions.setHideCart());
@@ -23,8 +25,17 @@ const Cart = (props) => {
 
   return (
     <Overlay onClose={closeCartHandler}>
-      <h2 className={classes.title}>Shopping Cart</h2>
-      {CartItems}
+      <div className={classes.container}>
+        <div className={classes.title}>
+          <BsCart4 className={classes.titleIcon} />
+        </div>
+
+        {CartItems}
+        <div className={classes.total}>
+          <span>Total Price: </span>
+          <span className={classes.totalPrice}>$ {totalPrice}</span>
+        </div>
+      </div>
     </Overlay>
   );
 };
