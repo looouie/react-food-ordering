fa15a680abb1013a1c76f8c20850b60094e11a08
loyilulu <loyichan2@gmail.com>
2022-05-07 22:06:07 +0200

    Updated index.js to React 18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store/index";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <App />{" "}
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
