d140f619a1bd103138433ddd815312ec2c24f90f
loyilulu <loyichan2@gmail.com>
2022-05-07 21:53:19 +0200

    refactor Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,72 +1,8 @@
 import classes from "./Cart.module.css";
-import Overlay from "../UI/Overlay/Overlay";
-import CartItem from "./CartItem/CartItem";
-import { BsCart4 } from "react-icons/bs";
-import { useDispatch, useSelector } from "react-redux";
-import { showCartActions } from "../../store/showCart-slice";
-import { cartActions } from "../../store/cart-slice";
-import { useState } from "react";
+import CartList from "./CartList/CartList";
+import CartForm from "./CartForm/CartForm";
 
 const Cart = (props) => {
-  const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
-  const [orderPlaced, setOrderPlaced] = useState(false);
-
-  const cartList = useSelector((state) => state.cart.products);
-  const totalPrice = useSelector((state) => state.cart.totalPrice);
-
-  const closeCartHandler = () => {
-    dispatch(showCartActions.setHideCart());
-  };
-
-  const submitHandler = () => {
-    setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-      dispatch(cartActions.reset());
-      closeCartHandler();
-    }, 3000);
-  };
-
-  const CartItems = (
-    <ul>
-      {cartList.map((item) => {
-        return <CartItem item={item} />;
-      })}
-    </ul>
-  );
-
-  const cartIsEmpty = cartList.length === 0;
-
-  const LoadingSpinner = <div className={classes.loader}></div>;
-  const orderHasBeenPlaced = <span>Order has been place</span>;
-
-  return (
-    <Overlay onClose={closeCartHandler}>
-      {}
-      <div className={classes.container}>
-        <div className={classes.title}>
-          <BsCart4 className={classes.titleIcon} />
-        </div>
-        {cartIsEmpty ? <div className={classes.empty}>Cart is empty</div> : ""}
-        {isLoading && LoadingSpinner}
-        {orderPlaced && orderHasBeenPlaced}
-        {!cartIsEmpty && !isLoading && CartItems}
-        {!cartIsEmpty && !isLoading && (
-          <>
-            <div className={classes.total}>
-              <span>Total Price: </span>
-              <span className={classes.totalPrice}>$ {totalPrice}</span>
-            </div>
-            <div className={classes.buttons}>
-              <button onClick={closeCartHandler}>Close</button>
-              <button onClick={submitHandler}>Order</button>
-            </div>
-          </>
-        )}
-      </div>
-    </Overlay>
-  );
+  return <CartForm />;
 };
 export default Cart;
