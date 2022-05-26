61b4563addb0ee7bf6d333fedf71c51059815656
loyilulu <loyichan2@gmail.com>
2022-05-26 22:36:26 +0200

    Updated place order function

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
-const ApiKey =
-  "https://louie-bakery-default-rtdb.europe-west1.firebasedatabase.app/";
+const apiKey =
+  "https://louie-bakery-default-rtdb.europe-west1.firebasedatabase.app/order.json";
 
-const placeOrder = async (products, customer) => {
+export const placeOrder = async (products, customer) => {
   // ordernumber: something ++
   // products id, price, name, amount
   // customer detail: name, phone, pickup time
@@ -9,17 +9,13 @@ const placeOrder = async (products, customer) => {
   // temp solution
   const orderNo = Math.floor(100000000 + Math.random() * 900000000);
 
-  const response = await fetch(
-    ApiKey,
-    { method: "POST" },
-    {
-      body: {
-        orderNo,
-        products,
-        customer,
-      },
-    }
-  );
+  const response = await fetch(apiKey, {
+    method: "POST",
+    body: JSON.stringify({ orderNo: { products, customer } }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   if (!response.ok) {
     throw new Error("Order Fail!");
