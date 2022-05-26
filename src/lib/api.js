3827a964162d926d7af8fc89ef8d1380e504615f
loyilulu <loyichan2@gmail.com>
2022-05-26 22:47:55 +0200

    Updated request body

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,25 +1,27 @@
 const apiKey =
   "https://louie-bakery-default-rtdb.europe-west1.firebasedatabase.app/order.json";
 
-export const placeOrder = async (products, customer) => {
+export const placeOrder = async (body) => {
   // ordernumber: something ++
   // products id, price, name, amount
   // customer detail: name, phone, pickup time
 
   // temp solution
-  const orderNo = Math.floor(100000000 + Math.random() * 900000000);
+  //   const orderNo = Math.floor(100000000 + Math.random() * 900000000);
 
   const response = await fetch(apiKey, {
     method: "POST",
-    body: JSON.stringify({ orderNo: { products, customer } }),
+    body: JSON.stringify(body),
     headers: {
       "Content-Type": "application/json",
     },
   });
 
+  const data = await response.json();
+
   if (!response.ok) {
     throw new Error("Order Fail!");
   } else {
-    console.log(response.status);
+    console.log(data);
   }
 };
