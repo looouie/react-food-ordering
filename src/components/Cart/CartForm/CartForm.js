f4b5bfe05e315b32a6a544492d50981aecf06b35
loyilulu <loyichan2@gmail.com>
2022-05-26 22:35:07 +0200

    Added submit handler and useRef to get input value

--- src/components/Cart/CartForm/CartForm.js
+++ src/components/Cart/CartForm/CartForm.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { showCartActions } from "../../../store/showCart-slice";
 
@@ -5,15 +6,42 @@ import classes from "./CartForm.module.css";
 import Overlay from "../../UI/Overlay/Overlay";
 import { getDate, getHour, getMinute } from "../../utility/getDateTime.js";
 
-const CartForm = () => {
+const CartForm = (props) => {
+  const { hideModal, onSubmit } = props;
+
+  const nameRef = useRef();
+  const phoneRef = useRef();
+  const pickupDateRef = useRef();
+  const pickupHourRef = useRef();
+  const pickupMinRef = useRef();
+
   const dispatch = useDispatch();
 
-  // dispatch hideCart Action - okay
-  // loader - habdle in Cart
-  // dummy message show "order successfully - comfirm with sms etc."
   const submitHandler = (event) => {
-    console.log(event);
+    event.preventDefault();
+    console.log(nameRef.target.value);
+    console.log(phoneRef.current.value);
+    console.log(pickupDateRef.current.value);
+
+    if (
+      nameRef.current.value.trim() === "" ||
+      phoneRef.current.value.trim() === "" ||
+      pickupDateRef.current.value.trim() === ""
+    ) {
+      return;
+    }
+
+    const details = {
+      name: nameRef.current.value,
+      phone: phoneRef.current.value,
+      pickupDate: pickupDateRef.current.value,
+      pickupTime: `${pickupHourRef.current.value} : ${pickupMinRef.current.value}`,
+    };
+
     dispatch(showCartActions.setHideCart());
+
+    onSubmit(details);
+    // console.log(details);
   };
 
   const hideCartHandler = () => {
@@ -28,30 +56,36 @@ const CartForm = () => {
         <form className={classes.form}>
           <div className={classes.formItem}>
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" required></input>
+            <input type="text" id="name" ref={nameRef} required></input>
           </div>
           <div className={classes.formItem}>
             <label htmlFor="phone">Phone number:</label>
-            <input type="number" id="phone" required></input>
+            <input type="number" id="phone" ref={phoneRef} required></input>
           </div>
           <div className={classes.formItem}>
             <label htmlFor="date">
               Pick up date: <br />
               (30 days from tomorrow)
             </label>
-            <input type="date" id="date" min={getDate()} required></input>
+            <input
+              type="date"
+              id="date"
+              min={getDate()}
+              ref={pickupDateRef}
+              required
+            ></input>
           </div>
           <div className={classes.formItem}>
-            <label htmlFor="time">Pick up Time:</label>
+            <label htmlFor="hour">Pick up Time:</label>
             <div className={classes.options}>
-              <select name="time" id="time">
+              <select name="hour" id="hour" ref={pickupHourRef}>
                 {getHour().map((hour) => (
                   <option key={hour} value={hour}>
                     {hour}
                   </option>
                 ))}
               </select>
-              <select name="minute" id="miunte">
+              <select name="minute" id="miunte" ref={pickupMinRef}>
                 {getMinute().map((minute) => (
                   <option key={minute} value={minute}>
                     {minute}
