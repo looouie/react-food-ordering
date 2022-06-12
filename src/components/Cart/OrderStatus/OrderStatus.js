b42dd9700ef85f6bf013d199ff39519beb60bddb
loyilulu <loyichan2@gmail.com>
2022-06-12 12:32:13 +0200

    Added orderStatus component

--- src/components/Cart/OrderStatus/OrderStatus.js
+++ src/components/Cart/OrderStatus/OrderStatus.js
@@ -1,11 +1,15 @@
 import classes from "./OrderStatus.module.css";
 import Overlay from "../../UI/Overlay/Overlay";
-import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
+import {
+  AiOutlineCheckCircle,
+  AiOutlineCloseCircle,
+  AiOutlineExclamationCircle,
+} from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { showCartActions } from "../../../store/showCart-slice";
 
 const OrderStatus = (props) => {
-  const { hideModal, status, orderNumber } = props;
+  const { hideModal, status, orderId } = props;
 
   const dispatch = useDispatch();
 
@@ -13,12 +17,43 @@ const OrderStatus = (props) => {
     dispatch(showCartActions.setHideCart());
   };
 
+  const successOrder = (
+    <>
+      <AiOutlineCheckCircle
+        className={`${classes.titleIcon} ${classes.success}`}
+      />
+      <div className={classes.status}>Ordered successfully</div>
+      <div>order number: {orderId}</div>
+    </>
+  );
+
+  const failOrder = (
+    <>
+      <AiOutlineCloseCircle
+        className={`${classes.titleIcon} ${classes.fail}`}
+      />
+      <div className={classes.status}>Order failed</div>
+    </>
+  );
+
+  const unknownError = (
+    <>
+      <AiOutlineExclamationCircle
+        className={`${classes.titleIcon} ${classes.unknown}`}
+      />
+      <div className={classes.status}>Unknown Error</div>
+    </>
+  );
+
   return (
     <Overlay onClose={closeCartHandler}>
       <div className={classes.container}>
         <div className={classes.title}>
-          <AiOutlineCloseCircle className={classes.titleIcon} />
-          <div className={classes.status}>Order successfully</div>
+          {status === "success"
+            ? successOrder
+            : status === "fail"
+            ? failOrder
+            : unknownError}
         </div>
       </div>
     </Overlay>
